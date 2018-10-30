0946d5e1fb4a5e5db57954160054ae6316d7cd21
Chris Franklin <cf73021@gmail.com>
2018-10-30 20:10:32 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,26 +1,22 @@
-require './environment'
-require_relative 'app/models/pirate'
-require_relative 'app/models/ship'
 
-module FormsLab
-  class App < Sinatra::Base
-
-    get "/" do
-      erb :root
+require './environment'	require './environment'
+ module FormsLab	module FormsLab
+  class App < Sinatra::Base	  class App < Sinatra::Base
+     get '/' do
+    # code other routes/actions here	      erb :root
+     end
+  end
+end	    get '/new' do
+      erb :'pirates/new'
     end
-
-    get "/new" do
-      erb :"pirates/new"
-    end
-
-    post '/pirates' do
-      attributes = params[:pirate]
-      @pirate = Pirate.new(attributes[:name], attributes[:weight], attributes[:height])
-       @ships = attributes[:ships].collect do |ship|
-        Ship.new(ship[:name], ship[:type], ship[:booty])
+     post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+       params[:pirate][:ships].each do |details|
+        Ship.new(details)
       end
-       erb :"pirates/show"
+      @ships = Ship.all
+       erb :'pirates/show'
     end
 
   end
-end
+end 
